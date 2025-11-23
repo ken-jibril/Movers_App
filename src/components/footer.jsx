4c17cab7ae56d80fbf38f038f01cf79ea100d4a3
ken kagai <jibken80@gmail.com>
2025-11-23 19:58:04 +0300

    Add footer file and add responsiveness

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return ( 
+        <div className="">
+            <footer className="bg-gray-800 text-white py-6 mt-8">
+                <div className="container mx-auto px-4 text-center">
+                    <p className="text-sm sm:text-base md:text-lg lg:text-xl">
+                        &copy;2025 MoveEasy. All rights reserved.
+                    </p>
+                </div>
+            </footer>
+        </div>
+     );
+}
+
+export default Footer;
